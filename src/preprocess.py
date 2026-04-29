a9d6b0d42b10d6ffa90198a635f454509184165f
Tanya Deep <152463811+Tani-2005@users.noreply.github.com>
2026-04-29 21:14:53 +0530

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,10 +4,10 @@ from sklearn.ensemble import IsolationForest
 def load_and_clean_data(filepath):
     df = pd.read_csv(filepath)
     
-    # Basic: Date conversion
+    # Date conversion
     df['last_updated'] = pd.to_datetime(df['last_updated'])
     
-    # Advanced: Anomaly Detection using Isolation Forest
+    # Anomaly Detection using Isolation Forest
     iso = IsolationForest(contamination=0.02, random_state=42)
     df['is_anomaly'] = iso.fit_predict(df[['temperature_celsius', 'precip_mm']])
     df['is_anomaly'] = df['is_anomaly'].apply(lambda x: 1 if x == -1 else 0)
